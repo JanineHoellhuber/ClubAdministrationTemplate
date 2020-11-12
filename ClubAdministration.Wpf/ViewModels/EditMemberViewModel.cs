040ea8ac14b902035629f833434888b33de5adcb
j07122000 <janine.hoellhuber@gmx.at>
2020-11-12 21:33:34 +0100

    Validierung

--- ClubAdministration.Wpf/ViewModels/EditMemberViewModel.cs
+++ ClubAdministration.Wpf/ViewModels/EditMemberViewModel.cs
@@ -72,20 +72,39 @@ namespace ClubAdministration.Wpf.ViewModels
                     _cmdSave = new RelayCommand(
                         execute: async _ =>
                         {
-                            using IUnitOfWork uow = new UnitOfWork();
-                            Member memberInDb = await uow.MemberSectionRepository.GetMemberByIdAsync(_member.Id);
-                            memberInDb.FirstName = Firstname;
-                            memberInDb.LastName = Lastname;
-                            uow.MemberSectionRepository.Update(memberInDb);
-                            await uow.SaveChangesAsync();
-                            Controller.CloseWindow(this);
+                            try
+                            {
+                                using IUnitOfWork uow = new UnitOfWork();
+                                Member memberInDb = await uow.MemberSectionRepository.GetMemberByIdAsync(_member.Id);
+                                memberInDb.FirstName = Firstname;
+                                memberInDb.LastName = Lastname;
+                                uow.MemberSectionRepository.Update(memberInDb);
+                                await uow.SaveChangesAsync();
+                                Controller.CloseWindow(this);
+                            }
+                            catch (ValidationException ve)
+                            {
+                                if (ve.Value is IEnumerable<string> properties)
+                                {
+                                    foreach (var property in properties)
+                                    {
+                                        AddErrorsToProperty(property, new List<string> { ve.ValidationResult.ErrorMessage });
+                                    }
+                                }
+                                else
+                                {
+                                    DbError = ve.ValidationResult.ToString();
+                                }
+                            }
                         },
-                        canExecute: _ => _member != null);
-                    
+
+                    canExecute: _ => IsValid
+                        );
                 }
                 return _cmdSave;
+
+            
             }
-            set { _cmdSave = value; }
 
         }
 
